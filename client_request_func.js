a619bfdc9bd0948c802728578b416df5c8112378
ASH-TOOLS <113542143+ASH-TOOLS@users.noreply.github.com>
2022-11-06 04:48:50 +0300

    Update client_request_func.js

--- client_request_func.js
+++ client_request_func.js
@@ -1,1 +1,18 @@
 
+function get_url_params(){
+  
+  var params_split = '/hi?hi=a&b=d'.split('?')[1].split('&');
+
+  var params={}
+
+  params_split.forEach(
+    
+      function(value){
+      
+        key_val = value.split('=')
+        params[key_val[0]] = key_val[1];
+      }
+  )
+  return params;
+  
+}
